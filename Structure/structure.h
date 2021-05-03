a9efd9cd7caefcf4ae92837bfe67a75e3aa54359
duykhanhrk <tranduykhanh.rk@gmail.com>
2021-05-03 17:10:37 +0700

    Fix(invoice_detail): rename a remove method - sec

---
+++ Structure/structure.h
@@ -0,0 +1,12 @@
+/* structure */
+
+#ifndef __STRUCTURE__
+#define __STRUCTURE__
+
+#include "support/message/message.h"
+#include "supplies/supplies.h"
+#include "invoice_detail/invoice_detail.h"
+#include "invoice/invoice.h"
+#include "staff/staff.h"
+
+#endif
--- program.cpp
+++ program.cpp
@@ -1,9 +1,6 @@
 /* Program */
 
-#include "structure/supplies/supplies.h"
-#include "structure/invoice_detail/invoice_detail.h"
-#include "structure/invoice/invoice.h"
-#include "structure/staff/staff.h"
+#include "structure/structure.h"
 
 int main() {
 
